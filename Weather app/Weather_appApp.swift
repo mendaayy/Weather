b6e0a49836885000d9f3965b2de609e737248f1b
Mendy <mendydnem@hotmail.com>
2023-03-09 10:41:44 +0100

    Hardcoded version UI

---
+++ Weather app/Weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appApp.swift
+//  Weather app
+//
+//  Created by mendy liang on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
